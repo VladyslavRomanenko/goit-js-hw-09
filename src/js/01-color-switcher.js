1de49da9c4a73aac21a774505355f38da24ac970
Vladyslav <vladyslavromanenkoo@gmail.com>
2023-05-04 13:24:47 +0300

    first task done

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,28 @@
+const startRef = document.querySelector('[data-start]');
+const stopRef = document.querySelector('[data-stop]');
+const bodyRef = document.querySelector('body');
 
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215)
+    .toString(16)
+    .padStart(6, 0)}`;
+}
+let intervalId;
+
+const startChangeColor = () => {
+  startRef.disabled = true;
+  stopRef.disabled = false;
+  intervalId = setInterval(() => {
+    bodyRef.style.backgroundColor = getRandomHexColor();
+  }, 1000);
+};
+
+const stopChangeColor = () => {
+  startRef.disabled = false;
+  stopRef.disabled = true;
+
+  clearInterval(intervalId);
+};
+
+startRef.addEventListener('click', startChangeColor);
+stopRef.addEventListener('click', stopChangeColor);
