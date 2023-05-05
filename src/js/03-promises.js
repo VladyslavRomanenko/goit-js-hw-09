383a512e661be4bd18dcf81c706162def0b2c377
Vladyslav <vladyslavromanenkoo@gmail.com>
2023-05-05 09:28:58 +0300

    third done

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -2,7 +2,7 @@ import Notiflix from 'notiflix';
 
 const form = document.querySelector('.form');
 
-function createPromise(position, delay, step) {
+function createPromise(position, delay) {
   const shouldResolve = Math.random() > 0.3;
   return new Promise((resolve, reject) => {
     setTimeout(() => {
@@ -11,6 +11,30 @@ function createPromise(position, delay, step) {
       } else {
         reject({ position, delay });
       }
-    });
+    }, delay + Math.random() * delay * 0.2);
   });
 }
+
+form.addEventListener('submit', event => {
+  event.preventDefault();
+  const delay = parseInt(form.elements.delay.value);
+  const step = parseInt(form.elements.step.value);
+  const amount = parseInt(form.elements.amount.value);
+  for (let i = 0; i < amount; i++) {
+    createPromise(i, delay + i * step)
+      .then(({ position, delay }) => {
+        Notiflix.Notify.success(
+          `✅ Fulfilled promise ${position} in ${delay}ms`
+        );
+      })
+      .catch(error => {
+        console.error(error);
+        Notiflix.Notify.failure(
+          `❌ Rejected promise ${error.position} in ${error.delay}ms`
+        );
+      });
+  }
+});
+
+const shouldResolve = Math.random() > 0.3;
+console.log(shouldResolve);
